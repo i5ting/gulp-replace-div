03a1b5088c574f9be71190c3226596b818739194
i5ting <i5ting@126.com>
2016-11-01 20:47:20 +0800

    lazycommmit, please ignore

--- test.js
+++ test.js
@@ -1,16 +1,18 @@
-// 'use strict'
-//
-// import test from 'ava'
-//
-// test('my test', t => {
-//   t.is(3, 3)
-// })
+'use strict'
+
+import test from 'ava'
 
 var fs = require('fs')
 var a = fs.readFileSync('fixture.md').toString()
-
 var repl = require('./replace_div')
+  
+test('replace_div.reg() test', t => {
+  t.regex(a, repl.reg('zh'), '.zh exist')
+  t.regex(a, repl.reg('en'), '.en exist')
+})
 
-var result = repl(a, 'en')
-
-console.log(result)
+test('replace_div() test', t => {
+  var result = repl(a, 'en')
+  t.regex(result, repl.reg('zh'), '.zh exist')
+  t.notRegex(result, repl.reg('en'), '.en not exist')
+})
